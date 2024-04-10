68927a4d4c497128f38f197b136ef5aa91ee3a86
RavinduWM <ravinduwije4@gmail.com>
2024-04-11 00:43:27 +0530

    implement of food cart page

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,4 +1,7 @@
+import 'package:eat_more/widget/widget_support.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Order extends StatefulWidget {
   const Order({super.key});
@@ -10,6 +13,105 @@ class Order extends StatefulWidget {
 class _OrderState extends State<Order> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Material(
+              elevation: 2.0,
+              child: Container(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Center(
+                  child: Text("Food Cart",
+                      style: AppWidget.HeadlineTextFieldStyle()),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(10.0),
+                child: Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10.0)),
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 70.0,
+                        width: 30.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Center(child: Text("2")),
+                      ),
+                      SizedBox(width: 20.0),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(60.0),
+                        child: Image.asset("images/food.jpg",
+                            width: 70.0, height: 70.0, fit: BoxFit.cover),
+                      ),
+                      SizedBox(width: 40.0),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Fries",
+                                style: AppWidget.semiBoldTextFieldStyle()),
+                            Text("Rs. 1500.00",
+                                style: AppWidget.semiBoldTextFieldStyle())
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Spacer(),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total Price",
+                    style: AppWidget.boldTextFieldStyle(),
+                  ),
+                  Text(
+                    "Rs.1500.00",
+                    style: AppWidget.semiBoldTextFieldStyle(),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(10.0)),
+                margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+                child: Center(
+                    child: Text("CheckOut",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold)))),
+          ],
+        ),
+      ),
+    );
   }
 }
