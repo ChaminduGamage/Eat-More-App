960fead47a46cea58f04a0f5a8b1db892f3e4de3
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-04-12 01:40:11 +0530

    feat: Session Route Protection

--- lib/admin/home_admin.dart
+++ lib/admin/home_admin.dart
@@ -1,7 +1,8 @@
 import 'package:eat_more/admin/add_food.dart';
+import 'package:eat_more/pages/login.dart';
 import 'package:eat_more/widget/widget_support.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomeAdmin extends StatefulWidget {
   const HomeAdmin({super.key});
@@ -18,57 +19,114 @@ class _HomeAdminState extends State<HomeAdmin> {
         margin: const EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         child: Column(
           children: [
-            Center(
-              child: Text(
-                "Home Admin",
-                style: AppWidget.HeadlineTextFieldStyle(),
-              ),
-            ),
-            const SizedBox(
-              height: 50.0,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const AddFood()));
-              },
-              child: Material(
-                elevation: 10.0,
-                borderRadius: BorderRadius.circular(10),
-                child: Center(
-                  child: Container(
-                    padding: const EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(10),
+            Expanded(
+              child: Column(
+                children: [
+                  Center(
+                    child: Text(
+                      "Home Admin",
+                      style: AppWidget.HeadlineTextFieldStyle(),
                     ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: Image.asset(
-                            "images/food.jpg",
-                            height: 100,
-                            width: 100,
-                            fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AddFood()));
+                    },
+                    child: Material(
+                      elevation: 10.0,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Center(
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(6.0),
+                                child: Image.asset(
+                                  "images/food.jpg",
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 30.0,
+                              ),
+                              const Text(
+                                "Add Food Items",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
                           ),
                         ),
-                        const SizedBox(
-                          width: 30.0,
-                        ),
-                        const Text(
-                          "Add Food Items",
-                          style: TextStyle(
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: GestureDetector(
+                onTap: () async {
+                  await FirebaseAuth.instance.signOut();
+                  // Clear all sessions
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LogIn()),
+                    (route) => false, // Prevents going back to previous routes
+                  );
+                },
+                child: Material(
+                  elevation: 10.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Icon(
+                              Icons.logout,
                               color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold),
-                        )
-                      ],
+                              size: 30.0,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30.0,
+                          ),
+                          Text(
+                            "Log Out",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
